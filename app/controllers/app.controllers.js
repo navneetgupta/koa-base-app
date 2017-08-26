225b78caaa0e1bea92327440d6487641f62cbe68
Navneet Gupta <Navneet-gupta01@users.noreply.github.com>
2017-08-26 19:38:33 +0400

    locals to state change

--- app/controllers/app.controllers.js
+++ app/controllers/app.controllers.js
@@ -4,31 +4,32 @@
 'use strict';
 
 module.exports.getBooks = async (ctx,next) => {
-    console.log(ctx.path);
-    console.log("1st middleware");
-    if(!ctx.locals) ctx.locals = {};
-    ctx.locals.data1 = {"book1" : [{
-        'author': 'Navneet Gupta',
-        'title' : 'Advance ES6'
-    },
-        {
-            'author': 'Navneet Gupta1',
-            'title' : 'Advance ES7'
-        },
-        {
-            'author': 'Navneet Gupta2',
-            'title' : 'Advance ES8'
-        }]};
+    if(!ctx.state) ctx.state = {};
+    ctx.state.data1 = {
+        "book1" : [{
+            'author': 'Navneet Gupta',
+            'title' : 'Advance ES6'
+            },
+            {
+                'author': 'Navneet Gupta1',
+                'title' : 'Advance ES7'
+            },
+            {
+                'author': 'Navneet Gupta2',
+                'title' : 'Advance ES8'
+            }
+        ]
+    };
     await next();
 };
 module.exports.getIndex = async (ctx,next) => {
-    if(!ctx.locals) ctx.locals = {};
-    ctx.locals.data1 = "Hi There!!!";
+    if(!ctx.state) ctx.state = {};
+    ctx.state.data1 = "Hi There!!!";
     await next();
 };
 
 module.exports.completeRequest = async (ctx,next) => {
-    if(ctx.locals.data1) {
-        ctx.body = ctx.locals.data1;
+    if(ctx.state.data1) {
+        ctx.body = ctx.state.data1;
     }
 };
